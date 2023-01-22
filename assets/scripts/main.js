673e9dcaf522dfc85a15a36abe032c137bf3f761
NavinPanthi <panthinabin341@gmail.com>
2023-01-22 13:05:48 +0545

    added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,40 @@
-function darkmode(){
-    if(document.body.style.backgroundColor=="black"){
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
-    }
-    else{
-        document.body.style.backgroundColor="black";
-        document.body.style.color = "white";
-    }
-    console.log(document.body.style.backgroundColor);
+function darkmode() {
+  if (document.body.style.backgroundColor == "black") {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  } else {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  }
+  console.log(document.body.style.backgroundColor);
 }
 
+function swapdarkmode() {
+  if (document.getElementById("da").classList.contains("uil-moon")) {
+    document.getElementById("da").classList.add("uil-sun");
+    document.getElementById("da").classList.remove("uil-moon");
+  } else {
+    document.getElementById("da").classList.remove("uil-sun");
+    document.getElementById("da").classList.add("uil-moon");
+  }
+}
+
+let frontend = document.querySelector(".frontend-section table");
+let fshow = 0;
+let ui_ux = document.querySelector(".ui_ux-section table");
+let ushow = 0;
+let backend = document.querySelector(".backend-section table");
+let bshow = 0;
+
+function showHideFrontend(){
+  frontend.classList.toggle("hide");
+  fshow=1;
+}
+function showHideui_ux() {
+  ui_ux.classList.toggle("hide");
+  ushow = 1;
+}
+function showHideBackend() {
+  backend.classList.toggle("hide");
+  bshow = 1;
+}
